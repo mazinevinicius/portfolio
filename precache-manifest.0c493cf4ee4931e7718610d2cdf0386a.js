870c72eb81d8e81863e4a53af81c94b3b46fa640
Vinicius M. Mazine <mazinevinicius@gmail.com>
2020-08-10 22:18:10 -0300

    Updates

---
+++ precache-manifest.0c493cf4ee4931e7718610d2cdf0386a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962a15753e1f5f8d02bc8f61f336b5d3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2a4c886bda2b2ce45290",
+    "url": "/portfolio/static/css/2.cb36f21f.chunk.css"
+  },
+  {
+    "revision": "2a2f1cf2d7ade803fc6c",
+    "url": "/portfolio/static/css/main.477a517e.chunk.css"
+  },
+  {
+    "revision": "2a4c886bda2b2ce45290",
+    "url": "/portfolio/static/js/2.39be2aa4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.39be2aa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a2f1cf2d7ade803fc6c",
+    "url": "/portfolio/static/js/main.95735de3.chunk.js"
+  },
+  {
+    "revision": "ca1117c2305eacb930d3",
+    "url": "/portfolio/static/js/runtime-main.c5c35fdb.js"
+  }
+]);
